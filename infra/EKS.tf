1544ecdb9a5afc338eb218713e378e464880b96b
frank samuel <itysamy@gmail.com>
2026-09-24 04:21:21 +1200

    SG update

--- infra/EKS.tf
+++ infra/EKS.tf
@@ -1,13 +1,3 @@
-# EKS cluster
-#
-# Uses terraform-aws-modules/eks/aws module. Subnets and control-plane subnets
-# are taken from the VPC module. Addons (coredns/kube-proxy/vpc-cni) are
-# explicitly requested to ensure they get managed/updated.
-#
-# Data source aws_eks_cluster_auth provides a token and CA for Kubernetes/Helm
-# providers (configured in providers.tf).
-
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
@@ -22,6 +12,18 @@ module "eks" {
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets # worker node subnets
   control_plane_subnet_ids = module.vpc.intra_subnets   # control plane/infra subnets
+
+  # Security Group Rules: Allow all node-to-node traffic for CNI pod networking
+  node_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Allow all node to node traffic across all ports"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+  }
 
   # Explicitly manage core addons
   cluster_addons = {
