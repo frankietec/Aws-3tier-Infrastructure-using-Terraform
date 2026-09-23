1a8f51a21d590af5a96b61194c24e05d18f816a8
frank samuel <itysamy@gmail.com>
2026-09-24 05:28:44 +1200

    provider

--- infra/Providers.tf
+++ infra/Providers.tf
@@ -30,31 +30,13 @@ terraform {
       version = ">= 3.0"
     }
   }
-
-  # namecheap = {
-  #   source  = "namecheap/namecheap"
-  # }
 }
-
-
 
 provider "aws" {
   region = var.aws_region
 }
 
-
-# namecheap providers
-# provider "namecheap" {
-#   user_name = var.namecheap_username
-#   api_key   = var.namecheap_api_key
-#   client_ip = var.namecheap_client_ip
-#   sandbox   = false
-# }
-
-
 # EKS data sources
-# These fetch: - API endpoint - CA certificate - Authentication token
-
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
 
@@ -71,24 +53,16 @@ data "aws_eks_cluster_auth" "cluster" {
   ]
 }
 
-
-
-
-
-# kubernetes provider
-# Kubernetes provider for post-EKS resources
-# Kubernetes provider alias for post-EKS resources
+# Default Kubernetes Provider (Default for all kubernetes_* resources)
 provider "kubernetes" {
-  alias                  = "post_eks"
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
-
-# Helm provider using the aliased Kubernetes provider
+# Helm Provider using EKS credentials
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.cluster.token
